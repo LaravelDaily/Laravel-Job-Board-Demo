a795f4e777bd2ebde5d0e699f81e48a1581ec8e8
Povilas Korop <povilask007@gmail.com>
2019-06-10 14:24:49 +0300

    Fixed #1 seeds with permissions

--- database/seeds/PermissionRoleTableSeeder.php
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -10,9 +10,17 @@ class PermissionRoleTableSeeder extends Seeder
     {
         $admin_permissions = Permission::all();
         Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
-        $user_permissions = $admin_permissions->filter(function ($permission) {
+        $employee_permissions = $admin_permissions->filter(function ($permission) {
             return substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_';
         });
-        Role::findOrFail(2)->permissions()->sync($user_permissions);
+        Role::findOrFail(2)->permissions()->sync($employee_permissions);
+
+        $candidate_permissions = $admin_permissions->filter(function ($permission) {
+            return substr($permission->title, 0, 5) != 'user_'
+                && substr($permission->title, 0, 5) != 'role_'
+                && substr($permission->title, 0, 11) != 'permission_'
+                && substr($permission->title, 0, 8) != 'country_';
+        });
+        Role::findOrFail(3)->permissions()->sync($candidate_permissions);
     }
 }
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -7,20 +7,29 @@ class RolesTableSeeder extends Seeder
 {
     public function run()
     {
-        $roles = [[
-            'id'         => 1,
-            'title'      => 'Admin',
-            'created_at' => '2019-04-29 05:12:11',
-            'updated_at' => '2019-04-29 05:12:11',
-            'deleted_at' => null,
-        ],
+        $roles = [
             [
-                'id'         => 2,
-                'title'      => 'User',
+                'id'         => 1,
+                'title'      => 'Admin',
                 'created_at' => '2019-04-29 05:12:11',
                 'updated_at' => '2019-04-29 05:12:11',
                 'deleted_at' => null,
-            ]];
+            ],
+            [
+                'id'         => 2,
+                'title'      => 'Employee',
+                'created_at' => '2019-04-29 05:12:11',
+                'updated_at' => '2019-04-29 05:12:11',
+                'deleted_at' => null,
+            ],
+            [
+                'id'         => 3,
+                'title'      => 'Candidate',
+                'created_at' => '2019-04-29 05:12:11',
+                'updated_at' => '2019-04-29 05:12:11',
+                'deleted_at' => null,
+            ],
+        ];
 
         Role::insert($roles);
     }
